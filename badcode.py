1d9d08269e33984e8c06c32f299e6bb4e48dbbc8
kayd chen <kayd@Kayds-MacBook-Air.local>
2025-02-19 13:55:52 -0500

    Functions

--- badcode.py
+++ badcode.py
@@ -1,1 +1,40 @@
-#bad code for calc app
+def addition_function(a, b):
+    if type(a) == int and type(b) == int:
+        return a + b
+    elif type(a) == float and type(b) == float:
+        return a + b
+    elif type(a) == int and type(b) == float:
+        return a + b
+    elif type(a) == float and type(b) == int:
+        return a + b
+    else:
+        return "Invalid Input"
+
+class Calculator:
+    def init(self):
+        pass
+
+    def s(self, a, b):
+        if type(a) == int and type(b) == int:
+            return a - b
+        elif type(a) == float and type(b) == float:
+            return a - b
+        elif type(a) == int and type(b) == float:
+            return a - b
+        elif type(a) == float and type(b) == int:
+            return a - b
+        else:
+            return 0
+
+class multiplyy(Calculator):
+    def multiply(self, a, b):
+        if type(a) == int and type(b) == int:
+            return a * b
+        elif type(a) == float and type(b) == float:
+            return a * b
+        elif type(a) == int and type(b) == float:
+            return a * b
+        elif type(a) == float and type(b) == int:
+            return a * b
+        else:
+            return "Invalid Input"
