5b3b02e95e0a432865ea324279ca5634d631bb55
gwithcode <gavinwithers1@gmail.com>
2025-02-19 14:06:45 -0500

    fixed function

--- goodcode.py
+++ goodcode.py
@@ -7,21 +7,23 @@ def subtract(num1, num2): #function takes in two numbers as parameters and subtr
 def multiply(num1, num2): #function takes in two numbers as parameters and multiplies them together, returning the product
     return num1 * num2
 
+def get_numbers():
+    num1 = int(input("Enter your first number: ")) # get two numbers from user function
+    num2 = int(input("Enter your second number: "))
+    return num1, num2
+
 def main():
     choice = 0 #initialize user choice variable
     while(choice != 10):
         choice = int(input('Enter your option for the calculator. (1. Add, 2. Subtract, 3. Multiply, 10. Quit)')) #ask user to input choice, looping indefinitely until user inputs 10
         if(choice == 1):
-            num1 = int(input("Enter your first number"))
-            num2 = int(input("Enter your second number"))
+            num1, num2 = get_numbers()
             print('Result: ', add(num1, num2)) #if choice 1, add two numbers and print result
         elif(choice == 2):
-            num1 = int(input("Enter your first number"))
-            num2 = int(input("Enter your second number"))
+            num1, num2 = get_numbers()
             print('Result: ', subtract(num1, num2)) #if choice 2, subtract two numbers and print result
         elif(choice == 3):
-            num1 = int(input("Enter your first number"))
-            num2 = int(input("Enter your second number"))
+            num1, num2 = get_numbers()
             print('Result: ', multiply(num1, num2)) #if choice 3, multiply two numbers and print result
         elif(choice == 10):
             break #if choice 10, exit while loop
